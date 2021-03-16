ba249a9bc0b61d621ad7b8df5bef1744d830f5ce
silicone_girl <kaily.a.newman@gmail.com>
2021-03-16 11:18:30 +0000

    new solution

--- Bunny Working Locations Problem/bunny_google_solution.py
+++ Bunny Working Locations Problem/bunny_google_solution.py
@@ -1,15 +1,10 @@
 def solution(x, y):
-    working_space_matrix = [[1]]
-    while True:
-        if len(working_space_matrix) >= x:
-            if len(working_space_matrix[x-1]) >= y:
-                return str(working_space_matrix[x-1][y-1])
-        last_working_space = working_space_matrix[-1][-1]
-        for line in working_space_matrix:
-            last_working_space += 1
-            line.append(last_working_space)
-        last_working_space += 1
-        working_space_matrix.append([last_working_space])
+    # asum = 0
+    # for n in range(1, (x+y)):
+    #     asum += n
+    n = x+y-1
+    asum = int((n*(n+1))/2)
+    return str(asum-(y-1))
 
 assert solution(3,2) == "9"
 assert solution(5,10) == "96"
